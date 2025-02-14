4934ae2d702ba45559a583a1abe9ec89c7d65411
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2025-02-14 15:18:35 +0600

    Tree diagram using DecisionThreeRegressor model(RMSE+MSE calcualted)

--- logs_corr_ml.py
+++ logs_corr_ml.py
@@ -15,8 +15,8 @@ from sklearn.preprocessing import LabelEncoder
 
 # Reading CSV and printing Output
 df = pd.read_csv('Permdata.csv')
-# print(df)
-# print(df.columns)
+print(df)
+print(df.columns)
 
 
 
@@ -25,17 +25,17 @@ encoder = LabelEncoder() # Label encoding the target data
 df['LITHOLOGY_encoded'] = encoder.fit_transform(df['LITHOLOGY']) # Unique values are encoded with a number (fit and transform)
 inputs = df.drop(['LITHOLOGY','LITHOLOGY_encoded'],axis='columns')
 target=df['LITHOLOGY_encoded']
-# print(df[['LITHOLOGY', 'LITHOLOGY_encoded']].head(7))
+print(df[['LITHOLOGY', 'LITHOLOGY_encoded']].head(7))
 print(encoder.classes_) # Unique values of the target data (Return the classes labels as a list)
 print(dict(zip(encoder.classes_, range(len(encoder.classes_))))) # Create a dictionary for printing what lithology is encoded as what number
 
 
 # Printing Input and Target
-# print(inputs)
-# print(target)
+print(inputs) # Features
+print(target) # Label
 
 # Descrption of Dataset
-# print(df.describe())
+print(df.describe())
 print(inputs.corr())
 
 # Generating Coorelation heatmap
@@ -51,10 +51,14 @@ from sklearn.model_selection import train_test_split # Need to isntall sklearn b
 x_train, x_test, y_train, y_test = train_test_split(inputs,target,test_size=0.3,random_state=1) #x is for features and y is for label(Lithology)
 
 # Calling Random Forest Regressor model
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor 
+# from sklearn.ensemble import RandomForestClassifier
 
-model=RandomForestRegressor()
+from sklearn.model_selection import GridSearchCV # GridSearchCV is used to find the best hyperparameters (tuning) of the model
+
+model=RandomForestRegressor() #Calling Regressor model
+
+# model = RandomForestClassifier() #Calling Classifier model 
 
 # Training the model with train data set 
 model.fit(x_train,y_train)
@@ -75,3 +79,65 @@ print(y_pred_rounded) # Displaying the rounded predictions
 
 lithology_predicted = encoder.classes_[y_pred_rounded.astype(int)] # Mapping the rounded predictions to their corresponding lithology labels
 print(lithology_predicted) # Displaying the predicted lithology labels
+
+# Calculating the accuracy of the model
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+accuracy = accuracy_score(encoder.classes_[y_test], lithology_predicted)
+print(f"Accuracy: {accuracy}")  #Classifier model is more suitable for this dataset
+
+# Calculating mean squared error (must use regrrssor model as it works on continuous data)
+
+from sklearn.metrics import mean_squared_error
+# y_test = [3, 2, 1, 0, 2]    # Actual labels (encoded)
+# y_pred = [2.8, 2.1, 0.9, 0.2, 2.4]  # Predicted values from the regressor
+MSE = mean_squared_error(y_test,y_pred)
+print(f"Mean Squared Error: {MSE}")
+
+# Calculating Root Mean Squared Error (must use regressor model as it  works on continuous data)
+RMSE = np.sqrt(MSE)
+print(f"Root Mean Squared Error: {RMSE}")
+
+#Hyper-parameter of the RandomForest Model
+print(model.get_params())
+
+#Importing Decision Tree Regre
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import tree
+model_T = DecisionTreeRegressor()
+
+#Training the model with train data set
+model_T.fit(x_train,y_train)
+
+# Predicted target variables
+y_pred_T = model_T.predict(x_test) # Predicting the model with test data set (generated from train_test_split)
+
+print(y_pred_T)
+
+# Calculating Correlation Coefficient of the model from test data set
+corrCoff_T = model_T.score(x_test,y_test)
+print(f"Correlation Coefficient:{corrCoff_T}") #Return the coefficient of determination R^2 of the prediction.
+
+# Calculating mean squared error (must use regrrssor model as it works on continuous data)
+MSE_T = mean_squared_error(y_test,y_pred_T)
+print(f"Mean Squared Error: {MSE_T}")
+
+# Calculating Root Mean Squared Error (must use regressor model as it  works on continuous data)
+RMSE_T = np.sqrt(MSE_T)
+print(f"Root Mean Squared Error: {RMSE_T}")
+
+# Finding the feature importance
+feature_names = inputs.columns
+print(feature_names)
+print(model.feature_importances_)
+
+# Generating tree diagram
+fig = plt.figure(figsize=(50,40))
+_ = tree.plot_tree(model_T, feature_names=feature_names, filled=True,fontsize=6)
+plt.show()
+
+# Hyper Parameter of DT model 
+print(model_T.get_params())
+
+
+
